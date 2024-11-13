6d2d8fb063d87e2d185c764327b74a12abb3d4ea
Christian Düben <cdueben.ml+cran@proton.me>
2024-11-13 15:10:05 +0000

    version 1.0.0

---
+++ tests/testthat/test-flip.R
@@ -0,0 +1,6 @@
+# vector
+testthat::test_that("vector flip boolean", {
+  v <- cpp_vector(c(TRUE, FALSE))
+  testthat::expect_invisible(flip(v))
+  testthat::expect_equal(to_r(v), c(FALSE, TRUE))
+})
